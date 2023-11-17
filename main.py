dd6d7b35e31af3b0a2f98114d54e5a64aad48da7
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-11-17 13:50:49 -0300

    fix: descoment

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from management.tab_handler import calculate_tab
-""" from management.product_handler import (
+from management.product_handler import (
     get_product_by_id,
     get_products_by_type,
     add_product
     )
-"""
+
 
 if __name__ == "__main__":
-    """ print(get_products_by_type('drink'))
+    print(get_products_by_type('drink'))
     print(get_product_by_id(28))
-    print(add_product("lorem inpusm", 12.5, 5, "lorem ipsum", "drink")) """
+    print(add_product("lorem inpusm", 12.5, 5, "lorem ipsum", "drink"))
     print(calculate_tab([{"_id": 1, "amount": 5}, {"_id": 19, "amount": 3}]))
